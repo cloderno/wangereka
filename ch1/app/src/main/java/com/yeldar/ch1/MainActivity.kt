424a9a87a19404dfb758fe686d0813eec210431a
cloderno <cloderno@gmail.com>
2026-05-14 14:44:55 +0500

    LazyColumn and LazyRows

--- ch1/app/src/main/java/com/yeldar/ch1/MainActivity.kt
+++ ch1/app/src/main/java/com/yeldar/ch1/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.yeldar.ch1
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yeldar.ch1.ui.theme.Ch1Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +37,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             Ch1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                    Column()
+//                    Row()
+//                    Notification()
+                    LColumn(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -42,6 +63,92 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Ch1Theme {
-        Greeting("Android")
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LRow(modifier = Modifier.padding(1.dp))
+            LColumn(modifier = Modifier.padding(1.dp).weight(1f))
+            LColumn(modifier = Modifier.padding(1.dp).weight(1f))
+        }
+    }
+}
+
+@Composable
+fun Column() {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("C-Android")
+        Text("C-Kotlin")
+        Text("C-Compose")
+    }
+}
+
+@Composable
+fun Row() {
+    Row(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text("R-Android")
+        Text("R-Kotlin")
+        Text("R-Compose")
+    }
+}
+
+@Composable
+fun Notification() {
+    Box(
+        modifier = Modifier.size(100.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            modifier = Modifier.size(80.dp),
+            imageVector = Icons.Outlined.Notifications,
+            contentDescription = null,
+            tint = Color.Green
+        )
+        Text(text = "9")
+    }
+}
+
+@Composable
+fun LRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.Blue)
+    ) {
+        items(100) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "Item number: $it"
+            )
+        }
+    }
+}
+
+@Composable
+fun LColumn(
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+    ) {
+        items(100) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "Item number: $it"
+            )
+        }
     }
 }
